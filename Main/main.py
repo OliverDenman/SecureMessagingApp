00fb389d63524391102910dfca417a1e595972c5
Oliver Denman <62333198+OliverDenman@users.noreply.github.com>
2021-03-25 18:06:26 +0000

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -74,20 +74,3 @@ eel.start("index.html", size=(1200,800), mode="chrome", block=True)
 
 def ServerStart():
     pass
-
-#Hello, Test
-# {
-#     "auto_formatting": true,
-#     "autoformat_ignore":
-#     [
-#     ],
-#     "pep8_ignore":
-#     [
-#         "E501"
-#     ],
-#     "anaconda_linter_underlines": false,
-#     "anaconda_linter_mark_style": "none",
-#     "display_signatures": false,
-#     "disable_anaconda_completion": true,
-#     "python_interpreter": "/usr/local/bin/python3"
-# }
