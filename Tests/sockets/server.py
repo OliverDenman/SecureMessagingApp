ad1f5c387019970eb1c50e8c63d2359be8eb5d79
Oliver Denman <62333198+OliverDenman@users.noreply.github.com>
2021-03-27 12:36:39 +0000

    Update server.py

--- Tests/sockets/server.py
+++ Tests/sockets/server.py
@@ -35,7 +35,6 @@ clients = {}
 IP = socket.gethostname()
 PORT = 1217
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((IP, PORT))
 
 if __name__ == "__main__":
